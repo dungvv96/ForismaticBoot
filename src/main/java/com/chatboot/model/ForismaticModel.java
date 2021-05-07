87bf97e7d0a7679a61445168b366f35188747c0a
unknown <dung.vu@migitek.com>
2021-05-07 14:58:35 +0700

    update

---
+++ src/main/java/com/chatboot/model/ForismaticModel.java
@@ -0,0 +1,13 @@
+package com.chatboot.model;
+
+import lombok.Data;
+
+@Data
+public class ForismaticModel {
+
+	private String quoteText;
+	private String quoteAuthor;
+	private String senderName;
+	private String senderLink;
+	private String quoteLink;
+}
